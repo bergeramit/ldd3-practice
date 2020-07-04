818536e5a86e5a73e4f75db27835171b8691a174
Amit Berger <bergeramit@gmail.com>
2020-07-04 06:11:30 -0700

    added hello world

---
+++ hello_world/hello.c
@@ -0,0 +1,19 @@
+#include <linux/init.h>
+#include <linux/module.h>
+
+
+MODULE_LICENSE("Dual BSD/GPL");
+
+static int hello_init(void) {
+    printk(KERN_ALERT "Hello!!\n");
+    return 0;
+}
+
+
+static void hello_exit(void) {
+    printk(KERN_ALERT "Bye!!\n");
+}
+
+
+module_init(hello_init);
+module_exit(hello_exit);
